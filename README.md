1f263d08e0a17fa4274c915f211bca129034e545
TheGreatAxios <sawyer@dirtroad.dev>
2023-01-31 07:29:30 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vite + Vue + Metaport
+
+Bare bones integration of Metaport into Vue + Vite
+
+Notice** Currently using a pre-release version of Metaport
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Metaport } from "@skalenetwork/metaport/build/index"
+const metaport = new Metaport({
+  chains: ["mainnet", "elated-tan-skat", "honorable-steel-rasalhague"],
+  openOnLoad: true,
+  openButton: true,
+  
+})
+</script>
+
+<template>
+  <div id="metaport">
+    <h2>Vite/Vue with Basic SKALE Metaport Integration</h2>
+  </div>
+</template>
+
+<style scoped>
+  div {
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+    background:darkslateblue;
+    color: white;
+  }
+</style>
